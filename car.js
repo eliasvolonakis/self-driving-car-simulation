b252982eebd2cbaa43e72d79e1d2fc1b0b714b97
Elias Volonakis <envolonakis@gmail.com>
2022-05-24 07:49:54 -0400

    Completed Car Physics

--- car.js
+++ car.js
@@ -5,26 +5,64 @@ class Car {
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle=0
+
         this.controls = new Controls()
     }
 
     update(){
         if(this.controls.forward){
-            this.y -= 2;
+            this.speed += this.acceleration;
         } 
         if(this.controls.back){
-            this.y += 2;
+            this.speed -= this.acceleration;
         }
+        if(this.speed > this.maxSpeed){
+            this.speed = this.maxSpeed;
+        } if(this.speed < -this.maxSpeed / 2) {
+            this.speed =- this.maxSpeed / 2
+        }
+
+        if(this.speed > 0){
+            this.speed -= this.friction;
+        } 
+        if(this.speed < 0){
+            this.speed += this.friction;
+        }
+        if(Math.abs(this.speed) < this.friction){
+            this.speed = 0;
+        }
+        if(this.speed != 0){
+            const flip = this.speed > 0 ? 1:-1;
+            if(this.controls.left){
+                this.angle += 0.03 * flip;
+            }
+            if(this.controls.right){
+                this.angle -= 0.03 * flip;
+            }
+        }
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 
     draw(context) {
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(-this.angle);
+
         context.beginPath();
         context.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width, 
             this.height
         );
         context.fill();
+
+        context.restore();
     }
 }
--- main.js
+++ main.js
@@ -10,6 +10,7 @@ animate();
 
 function animate() {
     car.update();
+    canvas.height = window.innerHeight;
     car.draw(context);
     requestAnimationFrame(animate);
 }
